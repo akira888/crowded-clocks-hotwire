959a77b1b4262bd8197b8b67f910b41e14e29945
akira888 <a.cup.of.happiness@gmail.com>
2025-06-14 12:25:56 +0900

    controllerで時間を刻めるようにした

--- app/controllers/clocks_controller.rb
+++ app/controllers/clocks_controller.rb
@@ -1,7 +1,24 @@
 class ClocksController < ApplicationController
+  STANDARD_HAND_DEGREE = 90.0
   def index
     @cols = 10
-    @big_hand_deg = "#{90}deg"
-    @small_hand_deg = "#{270}deg"
+    @big_hand_deg = "#{minute_degree}deg"
+    @small_hand_deg = "#{hour_degree}deg"
+  end
+
+  private
+
+  def minute_degree
+    @minute_degree ||= (next_time.min * 6) + (next_time.sec * 0.1) + STANDARD_HAND_DEGREE
+  end
+
+  def hour_degree
+    @hour_degree ||= (next_time.hour % 12 * 30) + (next_time.min * 0.5) + STANDARD_HAND_DEGREE
+  end
+
+  def next_time
+    # １秒進める（つぎのポジションを返す）
+    @next_time ||= Time.now.advance(seconds: 1)
   end
 end
+
