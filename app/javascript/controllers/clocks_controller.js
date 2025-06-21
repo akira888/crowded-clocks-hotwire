ea0f53e2b2a951e5b407b8c41264367c0907e2e3
Akira Yagi <a.cup.of.happiness@gmail.com>
2025-06-21 15:14:35 +0000

    fix: 針アニメーションを1秒ごとに監視・deg対応

--- app/javascript/controllers/clocks_controller.js
+++ app/javascript/controllers/clocks_controller.js
@@ -13,13 +13,22 @@ export default class extends Controller {
   connect() {
     if (!this.hasIntervalOutlet) return;
     this.callInterval()
-    this.animateHands()
+    this.startHandAnimationTimer()
   }
 
   // 画面初期表示用
   intervalOutletConnected() {
     this.callInterval()
-    this.animateHands()
+    this.startHandAnimationTimer()
+  }
+
+  startHandAnimationTimer() {
+    if (this._handAnimationTimer) clearInterval(this._handAnimationTimer)
+    this._handAnimationTimer = setInterval(() => this.animateHands(), 1000)
+  }
+
+  disconnect() {
+    if (this._handAnimationTimer) clearInterval(this._handAnimationTimer)
   }
 
   callInterval() {
@@ -36,10 +45,8 @@ export default class extends Controller {
     // 03秒以降または33秒以降ならアニメーション
     if ((sec >= 3 && sec < 30) || (sec >= 33)) {
       this.handTargets.forEach(hand => {
-        const target = hand.dataset.fixedAngle
-        if (!target) return
-        // 目的角度
-        const to = `${target}deg`
+        const to = hand.dataset.fixedAngle // すでにdeg付き
+        if (!to) return
         // 現在の角度
         const from = hand.style.rotate || "0deg"
         // 残り秒数
@@ -49,7 +56,6 @@ export default class extends Controller {
         } else if (sec >= 33) {
           remain = 60 - sec
         }
-        // アニメーションduration（ms）
         const duration = remain * 1000
         hand.animate([
           { rotate: from },
@@ -58,7 +64,6 @@ export default class extends Controller {
           duration: duration,
           fill: "forwards"
         })
-        // style属性も更新
         hand.style.rotate = to
       })
     }
